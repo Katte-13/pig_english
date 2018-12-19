aa308464fa9baef59907d0f80bb861ee7cfd5996
Catalina <44431898+Katte-13@users.noreply.github.com>
2018-12-19 20:24:42 +0200

    Update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -1,36 +1,26 @@
-"""Pig Latin translation"""
+"""Pig English translation"""
 import re
 
 intro = '\n--> '
 
-while True:
-    word = input(intro)
-"""word = re.findall(r'\w+', text) I tried to split the words from a phrase, 
-    but is returning a list and is not accepted on the next level"""
-    result = no_consonants (word)
-    if result:
-        print (result)
-    else: print (first_vowel(word))
-
     
-    
-    def no_consonants(word): 
-""" Return the word with a sufix,
-if the word has no consonants""" 
+def no_consonants(word):
+# Return the word with a sufix, if the word has no consonants
     
     consonants = ('b', 'c', 'd', 'f', 'g',
               'h','j', 'k', 'l', 'm','n',
               'p','r', 's', 't', 'v', 'x', 'z', 'w')
 
     for consonant in consonants:
-        if consonant in list(word):
+        if consonant in word:
             return None
-    pig_latin = word +'yay'
-    return pig_latin 
+    pig_latin = word +'aya'
+    return pig_latin
+            
+        
 
-    def first_vowel(word): 
- """Return the word until the first vowel and name it "prefix".
-    Name the rest of the word "stem". Switch them and add 'ay'."""
+def first_vowel(word):
+#Return the word until the first vowel and name it "prefix".Name the rest of the word "stem". Switch them and add sufix.
       
     vowels = ('a', 'e', 'i', 'o', 'u', 'y')
     for vowel in vowels:
@@ -39,5 +29,12 @@ if the word has no consonants"""
             stem = word[(word.index(vowel))::]
             pig_latin1 = stem + prefix + 'ay'
             return pig_latin1
-        else: 
-            return None
+    else:
+        return None
+
+while True:
+    word = input(intro)
+    """word = re.findall(r'\w+', text)"""
+    result = no_consonants (word)
+    if result:
+        print (result)
