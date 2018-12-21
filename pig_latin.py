b2f40bd55be92dfe0fc0c50d02cffa3a68a1187f
Catalina <44431898+Katte-13@users.noreply.github.com>
2018-12-21 14:14:02 +0200

    Update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -1,26 +1,22 @@
-"""Pig English translation"""
-import re
+#Pig English translation
 
 intro = '\n--> '
-
-    
+ 
 def no_consonants(word):
-# Return the word with a sufix, if the word has no consonants
+"""Return the word with a sufix, if the word has no consonants"""
     
     consonants = ('b', 'c', 'd', 'f', 'g',
               'h','j', 'k', 'l', 'm','n',
               'p','r', 's', 't', 'v', 'x', 'z', 'w')
-
     for consonant in consonants:
         if consonant in word:
             return None
     pig_latin = word +'aya'
     return pig_latin
             
-        
-
+       
 def first_vowel(word):
-#Return the word until the first vowel and name it "prefix".Name the rest of the word "stem". Switch them and add sufix.
+"""Return the word until the first vowel and name it "prefix".Name the rest of the word "stem". Switch them and add sufix."""
       
     vowels = ('a', 'e', 'i', 'o', 'u', 'y')
     for vowel in vowels:
