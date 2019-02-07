2274a53ae0434d394d34b116634f0899107a5427
Catalina <44431898+Katte-13@users.noreply.github.com>
2019-02-07 15:56:10 +0200

    Add files via upload
    Now the input is tokenized.

--- pig_latin.py
+++ pig_latin.py
@@ -1,22 +1,26 @@
-#Pig English translation
+"""Pig English translation"""
+import nltk, re
+from nltk.tokenize import RegexpTokenizer
 
+#tokenize the input
 intro = '\n--> '
- 
+
+   
 def no_consonants(word):
-"""Return the word with a sufix, if the word has no consonants"""
+# Return the word with a sufix, if the word has no consonants
     
     consonants = ('b', 'c', 'd', 'f', 'g',
               'h','j', 'k', 'l', 'm','n',
               'p','r', 's', 't', 'v', 'x', 'z', 'w')
+
     for consonant in consonants:
         if consonant in word:
             return None
     pig_latin = word +'aya'
     return pig_latin
-            
-       
+
 def first_vowel(word):
-"""Return the word until the first vowel and name it "prefix".Name the rest of the word "stem". Switch them and add sufix."""
+#Return the word until the first vowel and name it "prefix".Name the rest of the word "stem". Switch them and add sufix.
       
     vowels = ('a', 'e', 'i', 'o', 'u', 'y')
     for vowel in vowels:
@@ -29,8 +33,12 @@ def first_vowel(word):
         return None
 
 while True:
-    word = input(intro)
-    """word = re.findall(r'\w+', text)"""
-    result = no_consonants (word)
-    if result:
-        print (result)
+    intro = input(intro)
+    tokens = nltk.tokenize.RegexpTokenizer("[\\w']+|[^\\w\\s]+").tokenize(intro)
+    text = nltk.Text(tokens)
+    #text = [w.lower for w in text]
+    for word in text:
+        result = no_consonants (word)
+        if result:
+            print(result)  
+        else: print(first_vowel(word))
